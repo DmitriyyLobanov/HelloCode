1f054c27c080aa957cda7acd3804c45fbada8cc0
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-21 22:51:30 +0400

    task003

---
+++ Task003/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// По заданному номеру дня недели вывести его название.
+
+
+string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+Console.WriteLine("Введите номер дня недели от 1 до 7:");
+int numberOfDay = int.Parse(Console.ReadLine());
+int iteracion = 0;
+
+
+if (numberOfDay <= week.Length)
+{
+    Console.WriteLine(week[numberOfDay - 1]);
+}
+else
+{
+    Console.WriteLine("Введено число которому не соответствует день недели.");
+}
+
+

c0c13c5ace86584c6b20a93931ab16787b9e2598
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-23 23:32:12 +0400

    Task006 Task007

---
+++ Task006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите проверяемое целое число:");
+int number = Int32.Parse(Console.ReadLine());
+
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} - чётное");
+}
+else
+{
+    Console.WriteLine($"Число {number} - нечетное");
+}
+
+
---
+++ Task007/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("Введите число N: ");
+int positiveNumber = Convert.ToInt32(Console.ReadLine());
+int negativeNumber = positiveNumber * (-1);
+
+
+while (negativeNumber <= positiveNumber)
+{
+    Console.Write(negativeNumber);
+    Console.Write(" ");
+    negativeNumber++;
+}

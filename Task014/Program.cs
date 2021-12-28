4d28f7096911b88e4f8e25dd0b220c3c3f214fd9
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-28 23:34:00 +0400

    Task014 complete

--- Task014/Program.cs
+++ Task014/Program.cs
@@ -3,9 +3,9 @@
 int number = new Random().Next(10, 999);
 string numberToString = Convert.ToString(number);
 int lenght = numberToString.Length;
+int find = 3;
 
-if ()
+Console.WriteLine($"Заданное число = {number}.");
+if (lenght > (find - 1)) Console.WriteLine($"Третья цифра числа {number} = {numberToString[find - 1]}.");
+else Console.WriteLine($"В числе {number} нет третьей цифры.");
 
-
-
-Console.WriteLine(number);

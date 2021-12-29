871bfebc8a54e7d18a5dd2f9057beeb2966d62d3
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-29 23:09:22 +0400

    Task015 added

---
+++ Task015/Program.cs
@@ -0,0 +1,41 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите число для проверки на кратность числам 7 и 23.");
+int inputNumber = Int32.Parse(Console.ReadLine());
+
+int checkingMultiplicity(int divisibleNumber, int dividerNumber)
+{
+    int result = 0;
+    if (divisibleNumber % dividerNumber == 0)
+    {
+        result = 0;
+        return result;
+    }
+    else
+    {
+        result = divisibleNumber % dividerNumber;
+        return result;
+    }
+    
+}
+
+
+int multiplisity7 = checkingMultiplicity(inputNumber, 7);
+int multiplisity23 = checkingMultiplicity(inputNumber, 23);
+
+if (multiplisity7 == 0) 
+{
+    Console.WriteLine($"Число {inputNumber} кратно числу 7.");
+}
+else 
+{
+    Console.WriteLine($"Число {inputNumber} не кратно числу 7");
+}
+if (multiplisity23 == 0)
+{
+    Console.WriteLine($"Число {inputNumber} кратно числу 23.");
+}
+else
+{
+     Console.WriteLine($"Число {inputNumber} не кратно числу 23.");
+}

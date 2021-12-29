6e53e4d1d43f0793336e76e5f01628b3d7628efb
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-29 23:28:09 +0400

    Task016 added

---
+++ Task016/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+
+string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+Console.WriteLine("Введите номер дня недели от 1 до 7:");
+int numberOfDay = int.Parse(Console.ReadLine());
+
+
+if (numberOfDay <= week.Length)
+{
+    if (numberOfDay == 6 || numberOfDay == 7)
+    {
+        Console.WriteLine($"День недели под номером {numberOfDay} является выходным днём({week[numberOfDay - 1]}.)");
+    }
+    else
+    {
+        Console.WriteLine($"День недели под номером {numberOfDay} не является выходным днём({week[numberOfDay - 1]}.)");
+    }
+    
+}
+else
+{
+    Console.WriteLine("Введено число которому не соответствует день недели.");
+}

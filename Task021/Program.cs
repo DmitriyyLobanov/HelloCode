3cd0e000bc04d652bf27048fe159fed4050ac4b6
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-03 20:21:50 +0400

    Task021 added

---
+++ Task021/Program.cs
@@ -0,0 +1,34 @@
+﻿// Программа проверяет пятизначное число на палиндром.
+
+Console.Clear();
+void CheckPalindrome(int arg)
+{
+    string number = Convert.ToString(arg);
+    char[] array = number.ToCharArray();
+    Array.Reverse(array);
+    string InvertedNumber = new string(array);
+
+    bool result = number.Equals(InvertedNumber);
+    if (result)
+    {
+        Console.WriteLine($"Число {arg} является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine($"Число {arg} не является палиндромом.");
+    }
+}
+
+
+inputNumber:
+Console.WriteLine("Введите число:");
+bool ResultOfParse = Int32.TryParse(Console.ReadLine(), out int number);
+if (ResultOfParse)
+{
+    CheckPalindrome(number);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод, попробуйте снова.");
+    goto inputNumber;
+}

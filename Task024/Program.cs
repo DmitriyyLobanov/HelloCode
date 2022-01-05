52c9921076d12395b0e43246d809eb6e7fdf10a2
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-05 22:13:33 +0400

    Tasks 024, 025 added

---
+++ Task024/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+int N = new Random().Next(1, 25);
+
+Console.Clear();
+Console.WriteLine($"Число N = {N}");
+for (int i = 1; i <= N; i++)
+{
+    double SquareOfN = Math.Pow(i, 3);
+    Console.WriteLine($"|{i}|{SquareOfN}|");
+}
---
+++ Task025/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел от 1 до А
+
+int A = new Random().Next(1, 100);
+int Sum = 0;
+
+Console.Clear();
+Console.WriteLine($"Число А = {A}.");
+for (int i = 1; i <= A; i++)
+{
+    Sum = Sum + i;
+
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {A} = {Sum}.");

ad223679866f37050d68bbc5a2ef4450acb0a293
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-05 21:44:04 +0400

    Task023 added

---
+++ Task023/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+
+int N = new Random().Next(1, 25);
+
+Console.Clear();
+Console.WriteLine($"Число N = {N}");
+for (int i = 1; i <= N; i++)
+{
+    double SquareOfN = Math.Pow(i, 2);
+    Console.WriteLine($"|{i}|{SquareOfN}|");
+}
+/* Console.WriteLine("_______"); */
+
+

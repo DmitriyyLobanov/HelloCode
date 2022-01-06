4da2bc58c0e504147a1055ac010a994173619947
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-06 15:01:44 +0400

    Task026 added

---
+++ Task026/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+int A = new Random().Next(1, 51);
+int B = new Random().Next(1, 11);
+int Pow = A;
+Console.WriteLine(A);
+Console.WriteLine(B);
+
+for (int i = 1; i < B; i++)
+{
+   Pow *= A;
+}
+
+
+Console.WriteLine(Pow);

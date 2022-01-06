16395cadfcac6eada2bfde037fae3c4fcc376f45
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-06 21:28:22 +0400

    TAsk029 added

---
+++ Task029/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+int N = new Random().Next(1, 15);
+int Factorial = 1;
+for (int i = 1; i <= N; i++)
+{
+    Factorial = Factorial * i;
+}
+
+
+Console.WriteLine($"Факториал числа {N} равен {Factorial}.");

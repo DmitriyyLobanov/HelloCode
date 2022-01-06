cc45b53bedf37496589c664a783ad1cb5b5652ed
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-06 20:10:19 +0400

    Task027 added

---
+++ Task027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить количество цифр в числе
+
+
+
+Console.Clear();
+InputNumber:
+Console.WriteLine("Введите число.");
+bool ResultOfParse = int.TryParse(Console.ReadLine(), out int number);
+if (ResultOfParse)
+{
+    string check = Convert.ToString(number);
+    Console.WriteLine($"Количество цифр во введённом числе {number} равно {check.Length}.");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод, попробуйте снова.");
+    goto InputNumber;
+}
+

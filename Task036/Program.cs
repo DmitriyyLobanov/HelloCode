4fff2bc7e7fa57306ea3311136fad2d17080e5ce
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-09 19:20:33 +0400

    Task036 -037

---
+++ Task036/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+
+Console.Clear();
+int[] array = new int[20];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+string s = string.Join(", ", array);
+
+int even = 0;
+int odd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        even++;
+    }
+    else
+    {
+        odd++;
+    }
+}
+
+Console.WriteLine($"Заданный массив чисел - ({s}).");
+Console.WriteLine($"Количество четных чисел = {even}, количество нечетных чисел = {odd}.");
+
+
+
+
---
+++ Task037/Program.cs
@@ -0,0 +1,18 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+Console.Clear();
+int[] array = new int[123];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 150);
+}
+string s = string.Join(", ", array);
+Console.WriteLine($"Заданный массив чисел - ({s}).");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if ((array[i] >= 10) && (array[i] <= 99))
+    {
+        count++;
+    }
+}
+ Console.WriteLine($"Количество чисел в массиве, принадлежащик к диапазону [10, 99] = {count}.");

b79067524100099ba88421a65d8c32557b54e7de
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-10 23:40:26 +0400

    Task039-040

---
+++ Task039/Program.cs
@@ -0,0 +1,39 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.Clear();
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+}
+string s = string.Join(", ", array);
+Console.WriteLine($"Заданный массив чисел - ({s}).");
+Console.WriteLine("Произведения пар чисел(первое - последнее, второе - предпоследнее и т.д).");
+
+for (int i = 0, j = array.Length - 1; i < array.Length && j > 0; i++, j--)
+{
+    if (i > j)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine(array[i] * array[j]);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Hello, World!");
---
+++ Task040/Program.cs
@@ -0,0 +1,33 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+Console.Clear();
+double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = (new Random().NextDouble()) * 5;
+    array[i] = Math.Round(array[i], 2);
+}
+string s = string.Join("; ", array);
+Console.WriteLine($"Заданный массив вещественных чисел - ({s}).");
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($"Максимальное число, входящее в массив = {max}, минимальное = {min}.");
+Console.WriteLine($"Разница между максимальным и минимальным = {Math.Round((max - min), 2)}.");
+
+
+
+
+//Console.WriteLine("Hello, World!");

4ea8a102317360b1de4573c6043b74ebdf5ebc19
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-13 23:53:49 +0400

    Tasks 041...042

---
+++ Task041/Program.cs
@@ -0,0 +1,39 @@
+﻿// Выяснить являются ли три числа сторонами треугольника 
+
+
+
+Console.WriteLine("Введите первое число.");
+bool ResultOfParse = int.TryParse(Console.ReadLine(), out int FirstNumber);
+while (ResultOfParse == false)
+{
+    Console.WriteLine("Некорректный ввод, попробуйте снова.");
+    ResultOfParse = int.TryParse(Console.ReadLine(), out FirstNumber);
+}
+
+Console.WriteLine("Введите второе число.");
+ResultOfParse = int.TryParse(Console.ReadLine(), out int SecondNumber);
+while (ResultOfParse == false)
+{
+    Console.WriteLine("Некорректный ввод, попробуйте снова.");
+    ResultOfParse = int.TryParse(Console.ReadLine(), out SecondNumber);
+}
+
+Console.WriteLine("Введите третье число.");
+ResultOfParse = int.TryParse(Console.ReadLine(), out int ThirdNumber);
+while (ResultOfParse == false)
+{
+    Console.WriteLine("Некорректный ввод, попробуйте снова.");
+    ResultOfParse = int.TryParse(Console.ReadLine(), out ThirdNumber);
+}
+
+
+if ((FirstNumber > (SecondNumber + ThirdNumber)) && (SecondNumber > (FirstNumber + ThirdNumber)) && (ThirdNumber > (FirstNumber + SecondNumber)))
+{
+    Console.WriteLine($"Числа {FirstNumber}, {SecondNumber}, {ThirdNumber} не могут являться длинами сторон треугольника.");
+}
+else
+{
+    Console.WriteLine($"Числа {FirstNumber}, {SecondNumber}, {ThirdNumber}  могут являться длинами сторон треугольника.");
+}
+
+
---
+++ Task042/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+
+int CountPositive = 0;
+int CountNegative = 0;
+string s = string.Empty;
+Console.WriteLine("Введите числа через ENTER, если вы завершили ввод чисел введите q.");
+
+
+
+while (s != "q")
+{
+    s = Convert.ToString(Console.ReadLine());
+    bool ResultOfParse = int.TryParse(s, out int number);
+    if (number > 0)
+    {
+        CountPositive++;
+    }
+    if (number < 0)
+    {
+        CountNegative++;
+    }
+}
+
+Console.WriteLine($"Количество положительных чисел = {CountPositive}, отрицательных = {CountNegative}");

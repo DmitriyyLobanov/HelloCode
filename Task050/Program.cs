ee45f23c5c8ab430ba7194912e4602f8f0097f49
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-16 20:50:02 +0400

    Task050

---
+++ Task050/Program.cs
@@ -0,0 +1,53 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+
+Console.Clear();
+
+int m = new Random().Next(3, 10);
+int n = new Random().Next(3, 10);
+
+int[,] matrix = new int[m, n];
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i, j] % 2 == 0)
+        {
+            matrix[i, j] *= -1;
+        }
+    }
+}
+
+Console.WriteLine();
+PrintArray(matrix);
+
+
+

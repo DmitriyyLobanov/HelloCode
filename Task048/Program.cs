e51eb06e727cd53ffc2302662948fe3687a0fe49
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-16 20:29:40 +0400

    Task048 - 049

---
+++ Task048/Program.cs
@@ -0,0 +1,41 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+Console.Clear();
+
+int m = new Random().Next(3, 10);
+int n = new Random().Next(3, 10);
+
+int[,] matrix = new int[m, n];
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
+
+
+
---
+++ Task049/Program.cs
@@ -0,0 +1,40 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.Clear();
+
+int m = new Random().Next(3, 10);
+int n = new Random().Next(3, 10);
+
+double[,] matrix = new double[m, n];
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = (new Random().NextDouble()) * 5;
+            array[i, j] = Math.Round(array[i, j], 2);
+        }
+    }
+}
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
+

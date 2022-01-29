01733e35b14d257e6ea8aedbe212097ccf179c6e
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-01-29 16:35:15 +0400

    additionally examplex added

---
+++ Example014_RecursionAgain/Program.cs
@@ -0,0 +1,55 @@
+﻿// Второе занятие по рекурсивным методам.
+Console.Clear();
+
+// 1. Собрать строку от a до b, a <= b.
+
+/* string SecuenceRec(int a, int b)
+{
+    if (a <= b) return  $"{a}" + ' ' + SecuenceRec(a + 1, b);
+    return string.Empty;
+}
+Console.WriteLine(SecuenceRec(1, 10)); */
+
+// 2. Сумма чисел о 1 до n.
+
+/* int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumRec(10)); */
+
+// 3. Факториал числа.
+
+/* int FactorialRec(int n)
+{
+    if ((n == 1) || (n == 0)) return 1;
+    else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialRec(10)); */
+
+// 4. Вычисление а**n.
+
+/* int PowerRec(int a, int n)
+{
+    return n == 0 ? 1 :  (PowerRec(a, n - 1) * a);
+    //if (n == 0) return 1;
+    //else return (PowerRec(a, n - 1) * a);
+}
+Console.WriteLine(PowerRec(2, 10)); */
+
+// 5. Перебор "слов", состоящих из T количества букв.
+int n = 1;
+void FindWordsRec(string alphabet, char[] word, int lenght = 0)
+{
+    if (lenght == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[lenght] = alphabet[i];
+        FindWordsRec(alphabet, word, lenght + 1);
+    }
+}
+FindWordsRec("аист", new char[4]);
---
+++ Example015_Directories/Program.cs
@@ -0,0 +1,35 @@
+﻿// Примеры работы с директориями, с использованием рекурсивных методов.
+
+// Пример 1. Получение файлов находящихся в папке.
+/* Console.Clear();
+string path = "C:/Users/Zver/Desktop/GeekBrains/C#/introductionC#/seminar1/HelloCode/Example002_HelloUser";
+DirectoryInfo di = new DirectoryInfo(path);
+Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    Console.WriteLine(fi[i].Name);
+} */
+
+// Полный обход директории.
+
+void CatalogInfo (string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent} {catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent} {files[i].Name}");
+    }
+}
+
+string path = @"C:\Users\Zver\Desktop\GeekBrains\C#\introductionC#\seminar1\HelloCode";
+CatalogInfo(path);
